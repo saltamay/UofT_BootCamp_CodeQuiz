57c6f446be9f5eb881cdb6bee0fe30e64f7fd490
Sal Tamay <sal.tamay@outlook.com>
2019-10-19 19:22:41 -0400

    Add player and show rankings

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -30,7 +30,6 @@ const QuizGame = () => {
   }
 
   const displayQuestion = () => {
-    // let index = 0;
     const question = questions.shift();
     console.log(questions);
     const quiz = $(`<div class="card" style="width: 18rem;">
@@ -82,24 +81,9 @@ const QuizGame = () => {
     }
   }
 
-  const displayScorecard = () => {
-    $('.quiz').hide();
-    $('.score-card').show();
-    $('.score-card').html(`<div class="card" style="width: 18rem;">
-  <div class="card-header">
-    Score: ${gameTime}
-  </div>
-  <ul class="list-group list-group-flush d-flex flex-direction-row">
-    <li class="list-group-item">${wrong}</li>
-    <li class="list-group-item">${correct}</li>
-    <li class="list-group-item">${(correct / total * 100).toFixed(0)}%</li>
-  </ul>
-</div>`)
-  } 
-
   const stopWatch = () => {
     if (totalTime === 0) {
-      gameTime = totalTime;
+      // gameTime = totalTime;
       endGame();
       $('.time').text(totalTime);
     }
@@ -111,7 +95,98 @@ const QuizGame = () => {
 
   const endGame = () => {
     displayScorecard();
+    $('#nameSubmit').on('click', handleInputSubmit);
   }
+
+  const displayScorecard = () => {
+    $('.quiz').hide();
+    $('.score-card').show();
+    $('.score-card').html(`<div class="card" style="width: 18rem;">
+  <div class="card-header">
+    Score: ${gameTime}
+    <div class="input-group mb-3">
+    <input type="text" class="form-control player" placeholder="Initials" aria-label="Username" aria-describedby="basic-addon1">
+    <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" id="nameSubmit">Submit</button>
+    </div>
+  </div>
+  </div>
+  <ul class="list-group list-group-flush d-flex flex-row">
+    <li class="list-group-item">${wrong}</li>
+    <li class="list-group-item">${correct}</li>
+    <li class="list-group-item">${(correct / total * 100).toFixed(0)}%</li>
+  </ul>
+</div>`)
+  } 
+
+  const handleInputSubmit = (e) => {
+
+    e.preventDefault();
+
+    $('.score-card').hide();
+    $('.score-table').show();
+
+    const playerName = $('.player').val();
+    const player = {
+      name: playerName,
+      score: gameTime
+    }
+    // console.log(player);
+    saveToLocalStorage(player);
+    displayRankings(player);
+  }
+
+  const displayRankings = (currentPlayer) => {
+    const players = JSON.parse(localStorage.getItem('players'));
+    sortArray(players);
+    // console.log(players);
+
+    const rankings = $(`<div class="card" style="width: 18rem;">
+                      <div class= "card-header">
+                        Score Table
+                      </div>
+                    </div >`);
+
+    const playerList = $('<ul class="list-group list-group-flush"></ul>');
+
+    players.forEach((player, index) => {
+      if (currentPlayer.name === player.name) {
+        playerList.append(`<li type="button" class="list-group-item font-weight-bold mt-3">${index + 1}. ${player.name} ${player.score}</li>`);
+      }else{
+        playerList.append(`<li type="button" class="list-group-item mt-3">${index + 1}. ${player.name} ${player.score}</li>`);
+      }
+    })
+    rankings.append(playerList);
+    $('.score-table').append(rankings);
+  }
+
+  // Array that sorts the players array by the score
+  const sortArray = (arr) => {
+    arr.sort((a, b) => {
+      const scoreA = a.score;
+      const scoreB = b.score;
+      if (scoreA < scoreB)
+        return 1
+      if (scoreA > scoreB)
+        return -1
+      return 0
+    })
+  }
+
+  // Saves the player to the LocalStorage
+  const saveToLocalStorage = (player) => {
+    if (localStorage.getItem('players') === null) {
+      const players = [];
+      players.push(player);
+      localStorage.setItem('players', JSON.stringify(players));
+    } else {
+      const players = JSON.parse(localStorage.getItem('players'));
+      players.push(player);
+      localStorage.setItem('players', JSON.stringify(players));
+    }
+  }
+
+  
 
   init();
 }
