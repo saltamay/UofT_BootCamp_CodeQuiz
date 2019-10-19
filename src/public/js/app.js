6eee4697e1e44227d66d76eadd5a3a5810d48bf9
Sal Tamay <sal.tamay@outlook.com>
2019-10-19 17:34:57 -0400

    Setup initial game logic This commit sets up QuizGAme function that includes the following function:   - init() => Starts the game when start button is clicked   - startGAme() => Displays the quiz questions and listens for the answer   - handleAnswerClick() => Checks whether the answer is correct or not,     this function is basically the game logic   - displayScoreCard() => Displays the number of right and wrong answer,     percentage of correct answers and the displayScoreCard   - stopWatch() => timer   - endGame() => Finalizes the game

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,35 +1,131 @@
-/** Functions */
+/** App Function */
 
-const init = () => {
+const QuizGame = () => {
+  
+  // Set time
+  let totalTime = 10;
+  let gameTime = 0;
 
-}
+  // Set scores
+  let correct = 0;
+  let wrong = 0;
+  const total = questions.length;
 
-const displayQuestion = (question) => {
-  const quiz = $(`<div class="card" style="width: 18rem;">
+  const init = () => {
+    $(document).ready(function () {
+      // console.log(questions);
+      $('.start').on('click', startGame);
+    })
+  }
+
+  const startGame = (e) => {
+    e.preventDefault();
+    // Hide intro and display quiz questions
+    $('.intro').hide();
+    $('.quiz').show();
+
+    displayQuestion();
+    $('.time').text(totalTime);
+    stopWatch();
+  }
+
+  const displayQuestion = () => {
+    // let index = 0;
+    const question = questions.shift();
+    console.log(questions);
+    const quiz = $(`<div class="card" style="width: 18rem;">
                       <div class= "card-header">
                         ${question.title}
                       </div>
                     </div >`);
 
-  const choicesList = $('<ul class="list-group list-group-flush"></ul>');
+    const choicesList = $('<ul class="list-group list-group-flush"></ul>');
 
-  question['choices'].forEach(choice => {
-    choicesList.append(`<li type="button" class="list-group-item">${choice}</li>`);
-  })
-  quiz.append(choicesList);
-  $('.quiz').append(quiz);
+    choicesList.on('click', (e) => { handleAnswerClick(e, question.answer) })
+
+    question['choices'].forEach(choice => {
+      choicesList.append(`<li type="button" class="list-group-item mt-3">${choice}</li>`);
+    })
+    quiz.append(choicesList);
+    $('.quiz').append(quiz);
+  }
+  
+  const handleAnswerClick = (e, answer) => {
+    e.preventDefault();
+
+    if ($(e.target).html() === answer) {
+      e.target.style.backgroundColor = 'green';
+      correct++;
+      totalTime++;
+      setTimeout(() => {
+        $('.quiz').empty();
+        if (questions.length !== 0) {
+          displayQuestion();
+        }else {
+          gameTime = totalTime;
+          endGame();
+          $('.time').text(totalTime);
+          totalTime = 0;
+        }
+      }, 1000);
+    } else {
+      wrong++;
+      e.target.style.backgroundColor = 'red';
+      setTimeout(() => {
+        totalTime -= 9;
+        if (totalTime <= 0) {
+          totalTime = 0
+        }
+        $('.quiz').empty();
+        displayQuestion();
+      }, 1000);
+    }
+  }
+
+  const displayScorecard = () => {
+    $('.quiz').hide();
+    $('.score-card').show();
+    $('.score-card').html(`<div class="card" style="width: 18rem;">
+  <div class="card-header">
+    Score: ${gameTime}
+  </div>
+  <ul class="list-group list-group-flush d-flex flex-direction-row">
+    <li class="list-group-item">${wrong}</li>
+    <li class="list-group-item">${correct}</li>
+    <li class="list-group-item">${(correct / total * 100).toFixed(0)}%</li>
+  </ul>
+</div>`)
+  } 
+
+  const stopWatch = () => {
+    if (totalTime === 0) {
+      gameTime = totalTime;
+      endGame();
+      $('.time').text(totalTime);
+    }
+    if (totalTime > 0) {
+      $('.time').text(totalTime--);
+      setTimeout(stopWatch, 1000);
+    }
+  }
+
+  const endGame = () => {
+    displayScorecard();
+  }
+
+  init();
 }
 
-const startApp = (e) => {
-  e.preventDefault();
-  // Hide intro and display quiz questions
-  $('.intro').hide();
-  $('.quiz').show();
+QuizGame();
 
-  displayQuestion(questions[0]);
-}
 
-$(document).ready(function() {
-  $('.start').on('click', startApp);
-})
+
+
+
+
+
+
+
+
+
 
